91ee29e5437f527a71d2cc89e3f04b211e5fe2b2
Spenser Black <spenserblack01@gmail.com>
2021-10-14 14:31:55 -0400

    Support JSON response (#1)

--- api/index.rb
+++ api/index.rb
@@ -1,7 +1,14 @@
 Handler = Proc.new do |req, res|
   n = rand
 
-  res['Content-Type'] = 'text/plain; charset=utf-8'
   res.status = 200
+
+  if req['Accept'].include? 'application/json'
+    res['Content-Type'] = 'application/json; charset=utf-8'
+    res.body = %Q!{"value":#{n}}!
+    next
+  end
+
+  res['Content-Type'] = 'text/plain; charset=utf-8'
   res.body = n.to_s
 end
