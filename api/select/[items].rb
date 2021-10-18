1c739b9ae947458155128a243bfebe4912d4f119
Spenser Black <spenserblack01@gmail.com>
2021-10-18 10:45:00 -0400

    Add `select` API (#4)

---
+++ api/select/[items].rb
@@ -0,0 +1,17 @@
+Handler = Proc.new do |req, res|
+  separator = req.query['separator']
+  separator = ',' if separator.nil? || separator.empty?
+  items = req.query['items'].split separator
+  item = items.sample
+
+  res.status = 200
+
+  if req['Accept'].include? 'application/json'
+    res['Content-Type'] = 'application/json; charset=utf-8'
+    res.body = %Q!{"value":#{item}}!
+    next
+  end
+
+  res['Content-Type'] = 'text/plain; charset=utf-8'
+  res.body = item
+end
